8ec0546a9f1de9fb2f45e5dfdd5b1eca1bad995d
Sylvain <s.metayer@groupeonepoint.com>
2025-04-07 14:13:48 +0200

    wip: ajout de notes

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 # sudo dnf install inotify-tools
 # https://github.com/asciidoctor/asciidoctor-reveal.js/issues/248#issuecomment-629611778
 # https://github.com/asciidoctor/asciidoctor-reveal.js/issues/248#issuecomment-579277817
-while inotifywait -e close_write index.adoc slides js images diagrams css index-docinfo-footer.html; do
+while inotifywait -e close_write index.adoc slides js images diagrams css index-docinfo-footer.html docker-compose.yaml Dockerfile; do
   echo -n "Detected changes, rebuild presentation... "
-  docker compose up --build
+  docker compose up asciidoctor --build
   echo "Done !"
 done
