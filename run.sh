ed9daca9efbb41132566dc5c891e812c06eb5408
Sylvain <s.metayer@groupeonepoint.com>
2025-04-14 13:22:04 +0200

    fix: unnecessary file

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # sudo dnf install inotify-tools
 # https://github.com/asciidoctor/asciidoctor-reveal.js/issues/248#issuecomment-629611778
 # https://github.com/asciidoctor/asciidoctor-reveal.js/issues/248#issuecomment-579277817
-while inotifywait -e close_write index.adoc slides js images diagrams css index-docinfo-footer.html docker-compose.yaml Dockerfile; do
+while inotifywait -e close_write index.adoc slides js images diagrams css index-docinfo-footer.html docker-compose.yaml; do
   echo -n "Detected changes, rebuild presentation... "
   docker compose up asciidoctor --build
   echo "Done !"
